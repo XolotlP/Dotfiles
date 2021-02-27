0424d48430bd1f3ad7a4b0b12e28583f500c201d
XolotlP <xolotl.mp@gmail.com>
2021-02-27 14:53:39 -0600

    Update key-sync script
    - Check differences between directories at startup
    - Simplify sync() function
    - Make sure to use program executables instead of shell aliases
    - Make clear the invocation of inotifywait

--- scripts/key-sync/key-sync.sh
+++ scripts/key-sync/key-sync.sh
@@ -5,41 +5,44 @@
 #
 
 sync() {
-    origin=$1 dir_a=$2 dir_b=$3
+    origin=$1
 
-    if [[ $origin == $dir_a ]]; then
-        rsync -a --delete $dir_a $dir_b
-    else
-        rsync -a --delete $dir_b $dir_a
-    fi
+    destination=$([[ $origin == $cloud ]] && echo -n $keepass || echo -n $cloud)
+    /usr/bin/rsync -a --delete $origin $destination
 }
 
 keepass=/home/xolotl/.keepassxc/
 cloud=/home/xolotl/.nextcloud/.keepassxc/
-options="-q -m -e create -e delete -e modify -e move"
-pattern='.*\.kdbx\..+|#[0-9+]'
+events="-e create -e delete -e modify -e move"
+file_pattern='.*\.kdbx\..+|#[0-9+]'
 last_update=0
 
-if mkdir /tmp/key-sync.lock 2> /dev/null; then
+if /usr/bin/mkdir /tmp/key-sync.lock 2> /dev/null; then
     trap "rmdir /tmp/key-sync.lock 2> /dev/null; exit" INT TERM EXIT
-    notify-send "KeySync started" -i keysync
+
+    # notifications
+    /usr/bin/notify-send "KeySync started" -i keysync
     echo "KeySync started"
 
-    # it will only update databases when databases are in process of editing, it
-    # will not work if you are trying sync databases when keepassxc or nextcloud
-    # are not already running
+    # check differences at startup
+    # diff will return 0 (true) if there are not differences
+    /usr/bin/diff $keepass $cloud > /dev/null || dirs=($(ls -td $keepass $cloud))
+    [[ -n $dirs ]] && sync ${dirs[0]}
+
+    # watcher
     exec -a keysync \
-    inotifywait $options --format '%w %T' --timefmt %s --exclude $pattern $keepass $cloud | \
-    while read file mtime; do
+    /usr/bin/inotifywait --quiet --monitor --timefmt %s --format '%w %T' $events \
+    --exclude $file_pattern -- $keepass $cloud | \
+    while read mfile mtime; do
         if [[ $mtime -ge $last_update ]]; then
             sleep 1
             last_update=$(date +%s)
-            sync $file $keepass $cloud
+            sync $mfile
         fi
     done
-    rmdir /tmp/key-sync.lock
+    /usr/bin/rmdir /tmp/key-sync.lock
 else
-    pid=$(pidof keysync)
-    notify-send "KeySync is already running with PID=$pid" -i keysync
+    pid=$(/usr/bin/pidof keysync)
+    /usr/bin/notify-send "KeySync is already running with PID=$pid" -i keysync
     echo "Already running with PID=$pid"
 fi
